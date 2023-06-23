1ce82e48d4395aca55161886bc6a3806ca991d5a
AleksandrRiabinin <alexr2141@gmail.com>
2023-06-23 15:21:58 +0300

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import { useEffect, useState } from "react";
-import LifeGame from "./components/LifeGame";
 import { useDispatch, useSelector } from "react-redux";
 import { sizeAction } from "./redux/slices/cellSizeSlice";
 import { directionActions } from "./redux/slices/flexDirectionSlice";
 import Lifes from "./components/Lifes";
-import Input from "./components/common/Input";
-import InputResult from "./model/InputResult";
-import { countAction } from "./redux/slices/lifesCountSlice";
-import { useSelectorCount } from "./redux/store";
+
 
 const App: React.FC = () => {
   const dispatch = useDispatch<any>()
@@ -18,16 +14,8 @@ const App: React.FC = () => {
     })
   }, [])
 
-  let count = useSelectorCount()
-  function submitFn(inputText: string): InputResult {
-    const res: InputResult = {status: 'success'}
-    dispatch(countAction.setCount(+inputText))
-    return res;
-  }
-
   return <div>
-      {!count && <Input submitFn={submitFn} placeHolder="enter N lifes"></Input>}
-      {count != 0 && <Lifes lifesCount={count}/>}
+      <Lifes />
     </div>
 }
 export default App
--- src/components/Lifes.tsx
+++ src/components/Lifes.tsx
@@ -1,19 +1,33 @@
 import { useEffect } from "react";
-import { useSelectorDirection } from "../redux/store"
+import { useSelectorCount, useSelectorDirection } from "../redux/store"
 import { useDispatch } from "react-redux";
 import { countAction } from "../redux/slices/lifesCountSlice";
 import LifeGame from "./LifeGame";
+import Input from "./common/Input";
+import InputResult from "../model/InputResult";
 
-const Lifes: React.FC<{lifesCount: number}> = ({lifesCount}) => {
+const Lifes: React.FC = () => {
     
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(countAction.setCount(lifesCount));
     }, [])
+
+    const lifesCount = useSelectorCount()
+    function submitFn(input: string): InputResult {
+        const count = +input;
+        if (count < 1 || count > 4) {
+            return {status: "error", message: 'number sould be in range 1..4'}
+        }
+        dispatch(countAction.setCount(count))
+        return {status: 'success'};
+    }
     
     const flexDirection = useSelectorDirection();
+
     return <section style={{display: 'flex', flexDirection, alignItems: 'center', justifyContent: 'space-around'}}>
-        {lifesCount && Array.from({length: lifesCount}).map((_, index) => <LifeGame key={index} />)}
+        {!lifesCount && <Input submitFn={submitFn} placeHolder="enter N lifes (1..4)"></Input>}
+        {lifesCount  != 0 && Array.from({length: lifesCount}).map((_, index) => <LifeGame key={index} />)}
     </section>
 }
 export default Lifes
