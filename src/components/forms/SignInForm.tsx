8821b3f40e5c6af44d2f2f47f5a3c979195b4655
AleksandrRiabinin <alexr2141@gmail.com>
2023-07-10 15:42:45 +0300

    firebase auth

--- src/components/forms/SignInForm.tsx
+++ src/components/forms/SignInForm.tsx
@@ -10,7 +10,7 @@ import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import LoginData from '../../model/LoginData';
 import InputResult from '../../model/InputResult';
-import { Alert, Snackbar } from '@mui/material';
+import { Alert, Grid, Snackbar } from '@mui/material';
 import { StatusType } from '../../model/StatusType';
 
 const defaultTheme = createTheme();
@@ -19,16 +19,19 @@ type Props = {
     submitFn: (loginData: LoginData) => void
 }
 
-const SignInForm: React.FC<Props> = ({submitFn}) => {   
+const SignInForm: React.FC<Props> = ({ submitFn }) => {
 
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         const data = new FormData(event.currentTarget);
         const email: string = data.get('email')! as string
         const password: string = data.get('password')! as string
-        const res = await submitFn({email, password})
-
+        const res = await submitFn({ email, password })
     };
+
+    function googleSubmit() {
+        submitFn({ email: 'GOOGLE', password: '' })
+    }
 
     return (
         <ThemeProvider theme={defaultTheme}>
@@ -48,39 +51,60 @@ const SignInForm: React.FC<Props> = ({submitFn}) => {
                     <Typography component="h1" variant="h5">
                         Sign in
                     </Typography>
-                    <Box 
-                        component="form" 
-                        onSubmit={handleSubmit} 
+                    <Box
+                        component="form"
+                        onSubmit={handleSubmit}
                         noValidate sx={{ mt: 1 }}
                     >
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="email"
-                            label="Email Address"
-                            name="email"
-                            autoComplete="email"
-                            autoFocus
-                        />
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="password"
-                            label="Password"
-                            type="password"
-                            id="password"
-                            autoComplete="current-password"
-                        />
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}
-                        >
-                            Sign In
-                        </Button>
+                        <Grid container justifyContent={'center'} spacing={3}>
+                            <Grid item xs={12} sm={6} md={12}>
+                                <TextField
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    id="email"
+                                    label="Email Address"
+                                    name="email"
+                                    autoComplete="email"
+                                    autoFocus
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={6} md={12}>
+                                <TextField
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    name="password"
+                                    label="Password"
+                                    type="password"
+                                    id="password"
+                                    autoComplete="current-password"
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={6} md={12} >
+                                <Button
+                                    type="submit"
+                                    fullWidth
+                                    variant="contained"
+                                    sx={{ mt: 3, mb: 2 }}
+                                >
+                                    Sign In
+                                </Button>
+                            </Grid>
+                            <Grid item xs={12} sm={6} md={12}>
+                                <Button 
+                                    onClick={googleSubmit}
+                                    sx={{ mt: 3, mb: 2 }}
+                                >
+                                    Sign In With Google
+                                </Button>
+                            </Grid>
+                        </Grid>
+
+
+
+
+
                     </Box>
                 </Box>
             </Container>
--- src/service/auth/AuthServiceFire.ts
+++ src/service/auth/AuthServiceFire.ts
@@ -5,7 +5,8 @@ import {
     getFirestore, collection, getDoc, doc
 } from 'firebase/firestore';
 import {
-    getAuth, signInWithEmailAndPassword, signOut
+    GoogleAuthProvider,
+    getAuth, signInWithEmailAndPassword, signInWithPopup, signOut
 } from 'firebase/auth';
 import appFirebase from "../../config/firebase-config";
 
@@ -17,9 +18,13 @@ export default class AuthServiceFire implements AuthService {
     async login(loginData: LoginData): Promise<UserData> {
         let userData: UserData = null
         try {
-            const userAuth = await signInWithEmailAndPassword(this.auth, loginData.email, loginData.password);
+            const userAuth = loginData.email === 'GOOGLE' 
+                ? 
+                await signInWithPopup(this.auth, new GoogleAuthProvider())
+                :
+                await signInWithEmailAndPassword(this.auth, loginData.email, loginData.password);
             userData = { 
-                email: loginData.email,
+                email: userAuth.user.email as string,
                 role: await this.isAdmin(userAuth.user.uid) ? 'admin' : 'user'
             }
         } catch (error: any) {
